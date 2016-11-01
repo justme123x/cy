623e7d0a6fe89f5c543d82fb4e82c19f6e095597
pengchongyue <pengchongyue@chamerfood.com>
2016-11-01 14:22:59 +0800

    更新composer.json

--- app/Home/Controllers/IndexController.php
+++ app/Home/Controllers/IndexController.php
@@ -15,21 +15,24 @@ class IndexController extends \Cy\CyController
 
 		$obj = new \App\Model\Test();
 
-//		$cond = [];
-//		$cond['id'] = 1;
-//		$cond['ymd'] = ['>=' => 20151001, '<=' => 2015111];
-//		$cond['type'] = ['IN' => [1, 2, 3, 4, 5]];
-//		$cond['role'] = ['not in' => [1, 2, 3, 4, 5]];
-//		$cond['name'] = ['like' => '%san%'];
-//
-//		$data = [];
-//		$data['id'] = 5;
-//		$data['name'] = '张三';
-//		$obj->table('test');
-//		dump($obj->select(['id','name'])->where($cond)->findAll());
+		$cond = [];
+		$cond['id'] = 1;
+		$cond['ymd'] = ['>=' => 20151001, '<=' => 2015111];
+		$cond['type'] = ['IN' => [1, 2, 3, 4, 5]];
+		$cond['role'] = ['not in' => [1, 2, 3, 4, 5]];
+		$cond['name'] = ['like' => '%san%'];
+
+		$data = [];
+		$data['id'] = 5;
+		$data['name'] = '张三';
+		$obj->select(['id','name'])->where($cond)->findAll();
+
+		$obj->reset()->insert($data);
+
+		$obj->reset()->where($cond)->update($data);
 
 		$cond = ['id'=>1];
-		dump($obj->where($cond)->find());
+		$obj->reset()->where($cond)->find();
 	}
 
 
